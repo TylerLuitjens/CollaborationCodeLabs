05bed20c9b346e504582865f551131a6a926a13e
asc2 <asc2@10.9.34.38>
2014-11-20 13:23:27 -0600

    apothecary in progress

---
+++ AlphaAdventure/src/apothecary.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class apothecary {
+	
+	
+	public static String potions (String option, int coins)
+	{
+		Scanner it = new Scanner (System.in);
+		option = it.nextLine(); 
+		int HealthPotion = 20;
+		int AttackPotion = 15;
+		int DefencePotion = 15;
+		if(coins >= HealthPotion)
+		{
+			
+		}
+
+	}
+				
+}
