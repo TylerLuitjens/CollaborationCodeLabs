0da0d8cfcb0d6064be98ea6c9d7b7bf49f1b96ac
asc2 <asc2@10.9.34.38>
2014-11-24 13:38:21 -0600

    apothecary and potion tester

---
+++ AlphaAdventure/src/Inventory.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Inventory {
+
+	
+	public static  ArrayList<String> PotionCase(String potions)
+	{
+		ArrayList<String> pack = new ArrayList<String>();
+		pack.add(potions);
+		return pack;
+	}
+}
+
--- AlphaAdventure/src/apothecary.java
+++ AlphaAdventure/src/apothecary.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import static java.lang.System.*;
 
 public class apothecary {
 	
 	
-	public static String potions (String option, int coins)
+	public void potions (String option, int coins)
 	{
 		Scanner it = new Scanner (System.in);
 		option = it.nextLine(); 
 		int HealthPotion = 20;
 		int AttackPotion = 15;
 		int DefencePotion = 15;
-		if(coins >= HealthPotion)
+		String Heal = "";
+		String Atk = "";
+		String Def = "";
+		if(coins == (HealthPotion))
 		{
-			
+			Inventory.Backpack("nothing", Heal);
 		}
 
 	}
