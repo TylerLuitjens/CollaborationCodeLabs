3bc963f266b6e7b884bb858b35e5666577770158
asc2 <asc2@10.9.34.38>
2014-11-24 14:12:42 -0600

    test

---
+++ AlphaAdventure/src/AlphaAdventure/Dagger.java
@@ -0,0 +1,22 @@
+package AlphaAdventure;
+
+public class Dagger implements weapon {
+
+	@Override
+	public int durability() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int damage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
