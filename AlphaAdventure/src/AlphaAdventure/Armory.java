666aad5571e528a2bca5dc8b16e334e161fb134a
TylerLuitjens <tyluitjens@gmail.com>
2014-11-24 14:13:30 -0600

    Fine tuned the armory and test push with fast forward merge

--- AlphaAdventure/src/AlphaAdventure/Armory.java
+++ AlphaAdventure/src/AlphaAdventure/Armory.java
@@ -1,16 +1,20 @@
 package AlphaAdventure;
-
+import java.util.Scanner;
 public class Armory {
 	int cash;
 	String type;
 	weapon inhand;
+	Scanner scan=new Scanner(System.in);
 public Armory(String weapon,int money)
 {
 	type=weapon;
 	cash=money;
-	inhand = new gun();
+	System.out.println("What type of weapon do you wish to make? ");
+	if(scan.next().equalsIgnoreCase("Sword"));
+	inhand=new Sword();
 	
-	System.out.println(inhand.health());
+	
+	
 }
 public weapon Craft(String choice)
 {
