af0295cfc39724a1939afc2818ad208a45515713
asc2 <asc2@10.9.34.38>
2014-11-24 14:10:48 -0600

    test

---
+++ AlphaAdventure/src/AlphaAdventure/Flask.java
@@ -0,0 +1,15 @@
+package AlphaAdventure;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Flask {
+
+	
+	public static  ArrayList<String> PotionCase(String potions)
+	{
+		ArrayList<String> pack = new ArrayList<String>();
+		pack.add(potions);
+		String tester;
+		return pack;
+	}
+}
+
---
+++ AlphaAdventure/src/AlphaAdventure/Sword.java
@@ -0,0 +1,23 @@
+package AlphaAdventure;
+
+public class Sword implements weapon{
+
+	@Override
+	public int durability() {
+		// TODO Auto-generated method stub
+		return 150 ;
+	}
+
+	@Override
+	public int damage() {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+
+	@Override
+	public int cost() {
+		// TODO Auto-generated method stub
+		return 100;
+	}
+
+}
